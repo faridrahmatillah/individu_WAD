f485cdadc5742cf3527f2f3ff74b6cd8a2efdaf6
faridrahmatillah <faridrahmatillah972@gmail.com>
2026-09-15 20:40:35 +0700

    perbaikan

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
-const Home = () => {
+import Hero from "../component/Hero.jsx";
+
+function Home  ()  {
   return (
-    <div>
+    <div className= "text-purple-600">Home
       <h1>Home</h1>
       <p>Ini adalah halaman Home</p>
+      <section><Hero /></section>
     </div>
   );
 };
